1c350bfe3998b22eeb1ef0504cebb87e332fd4c8
Boriwan <43809289+Boriwan@users.noreply.github.com>
2026-09-09 14:27:28 +0200

    added a back button to den detail

--- resources/views/dens/show.blade.php
+++ resources/views/dens/show.blade.php
@@ -1,7 +1,10 @@
 <x-site-layout>
-<div>
-    <h2>{{ $den->name }}</h2>
-    <p>Created Date: {{ $den->created_date }}</p>
-    <p>User ID: {{ $den->user_id }}</p>
-</div>
+    <div style="margin: 20px;">
+        <button onclick="window.history.back()"
+            style="background-color: #f99d34; border: none; padding: 10px 20px; border-radius: 5px; cursor: pointer;">&larr;
+            Back</button>
+        <h2>{{ $den->name }}</h2>
+        <p>Created Date: {{ $den->created_date }}</p>
+        <p>User ID: {{ $den->user_id }}</p>
+    </div>
 </x-site-layout>
